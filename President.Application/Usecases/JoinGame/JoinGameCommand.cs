86fff9eaf16a6e200b7b98cdb1f6185f21851922
stevix77 <s.valentin77@gmail.com>
2021-12-21 21:46:30 +0100

    PlayerShouldJoinTheGam, GameShouldNotAddPlayerUnknown

---
+++ President.Application/Usecases/JoinGame/JoinGameCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace President.Application.Usecases.JoinGame
+{
+    public class JoinGameCommand
+    {
+        public JoinGameCommand(string playerId, string gameId)
+        {
+            PlayerId = playerId;
+            GameId = gameId;
+        }
+
+        public string GameId { get; }
+        public string PlayerId { get; }
+    }
+}
