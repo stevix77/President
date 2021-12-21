a595b1a1400b060657ec43178506321e1bf5e301
steve valentin <s.valentin77@hotmail.fr>
2021-12-21 22:16:04 +0100

    refactoring test

---
+++ President.Infrastructure/Repositories/InMemoryPlayerRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace President.Infrastructure.Repositories
+{
+    using President.Domain.Players;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class InMemoryPlayerRepository : IPlayerRepository
+    {
+        private readonly List<Player> _players;
+
+        public InMemoryPlayerRepository(Player player = null)
+        {
+            _players = new List<Player>();
+            if (player != null)
+                _players.Add(player);
+        }
+
+        public Task<Player> GetByIdAsync(PlayerId playerId)
+        {
+            return Task.FromResult(_players.FirstOrDefault(x => x.PlayerId.Equals(playerId)));
+        }
+    }
+}
