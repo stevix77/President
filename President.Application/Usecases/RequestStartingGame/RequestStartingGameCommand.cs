53953269063d1a71dfbc26b63556447a4ed894f8
steve valentin <s.valentin77@hotmail.fr>
2021-12-22 15:28:18 +0100

    PlayerCanRequestToStartAGame, PlayerCannotAskStartingGameWhenNotInGame

---
+++ President.Application/Usecases/RequestStartingGame/RequestStartingGameCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace President.Application.Usecases.RequestStartingGame
+{
+    public class RequestStartingGameCommand
+    {
+        public RequestStartingGameCommand(string playerId, string gameId)
+        {
+            GameId = gameId;
+            PlayerId = playerId;
+        }
+
+        public string GameId { get; }
+        public string PlayerId { get; }
+    }
+}
