7ae3e3015d0d9342f9e0338fbca7d9073fdb1531
stevix77 <s.valentin77@gmail.com>
2021-12-23 01:50:05 +0100

    PlayerCannotRequestStartingGameWhenGameIsStarted, PlayerUnknownCannotRequestStartingGame, refactoring

--- President.Domain/Games/GameState.cs
+++ President.Domain/Games/GameState.cs
@@ -7,16 +7,22 @@ namespace President.Domain.Games
         private readonly string _gameId;
         private readonly bool _hasBegan;
         private readonly Player[] _players;
+        private readonly PlayerId[] _startingRequests;
 
-        public GameState(string gameId, bool hasBegan, Player[] players)
+        public GameState(string gameId,
+                         bool hasBegan,
+                         Player[] players,
+                         PlayerId[] startingRequests)
         {
             _gameId = gameId;
             _hasBegan = hasBegan;
             _players = players;
+            _startingRequests = startingRequests;
         }
 
         public string GameId { get => _gameId; }
         public bool HasBegan { get => _hasBegan; }
         public Player[] Players { get => _players; }
+        public PlayerId[] StartingRequests { get => _startingRequests; }
     }
 }
