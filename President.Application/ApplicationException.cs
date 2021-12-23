ffd43657c44a783443be4a03ee1957fceef4e039
stevix77 <s.valentin77@gmail.com>
2021-12-23 01:50:05 +0100

    PlayerCannotRequestStartingGameWhenGameIsStarted, PlayerUnknownCannotRequestStartingGame, refactoring

---
+++ President.Application/ApplicationException.cs
@@ -0,0 +1,15 @@
+﻿namespace President.Application
+{
+    using System;
+    using System.Runtime.Serialization;
+
+
+    [Serializable]
+    public class ApplicationException : Exception
+    {
+
+        public ApplicationException(string message) : base(message)
+        {
+        }
+    }
+}
--- President.Domain/Games/Game.cs
+++ President.Domain/Games/Game.cs
@@ -2,6 +2,7 @@
 using President.Domain.Players;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace President.Domain.Games
@@ -20,18 +21,44 @@ namespace President.Domain.Games
             _startRequests = new Dictionary<PlayerId, bool>();
         }
 
-        private Game(GameId gameId, bool hasBegan, Player[] players)
+        private Game(GameId gameId,
+                     bool hasBegan,
+                     Player[] players,
+                     PlayerId[] startingRequests)
         {
             _gameId = gameId;
             _players = players.ToList();
             _hasBegan = hasBegan;
-            _startRequests = new Dictionary<PlayerId, bool>();
+            _startRequests = new Dictionary<PlayerId, bool>(
+                                    players.Select(x => new KeyValuePair<PlayerId, bool>(
+                                                    x.PlayerId, startingRequests.Contains(x.PlayerId))
+                                                   )
+                                    );
+        }
+
+        public static Game FromState(GameState gameState)
+        {
+            return new Game(new GameId(gameState.GameId),
+                            gameState.HasBegan,
+                            gameState.Players,
+                            gameState.StartingRequests);
+        }
+
+        internal bool HasBegan() => _hasBegan;
+
+        public GameId GameId { get => _gameId; }
+        public IReadOnlyCollection<Player> Players { get => _players; }
+        public IEnumerable<PlayerId> AcceptedStartRequests 
+        { 
+            get => _startRequests.Where(x => x.Value).Select(x => x.Key);
         }
 
         internal void AcceptRequestFromPlayer(Player player)
         {
             CheckRule(new PlayerMustBeInGameRule(player, this));
-            _startRequests[player.PlayerId] = true;
+            CheckRule(new GameMustBeNotStartedRule(this));
+            AcceptRequestWhenNotRequestedYet(player);
+
         }
 
         internal bool ContainsPlayer(Player player)
@@ -39,21 +66,17 @@ namespace President.Domain.Games
             return _players.Contains(player);
         }
 
-        public static Game FromState(GameState gameState)
-        {
-            return new Game(new GameId(gameState.GameId), gameState.HasBegan, gameState.Players);
-        }
-
-        internal bool HasBegan() => _hasBegan;
-
-        public GameId GameId { get => _gameId; }
-        public IReadOnlyCollection<Player> Players { get => _players; }
-        public List<PlayerId> AcceptedStartRequests { get => _startRequests.Keys.ToList(); }
-
         internal void AddPlayer(Player player)
         {
+            CheckRule(new GameMustBeNotStartedRule(this));
             _players.Add(player);
             _startRequests.Add(player.PlayerId, false);
+        }
+
+        private void AcceptRequestWhenNotRequestedYet(Player player)
+        {
+            if (!_startRequests[player.PlayerId])
+                _startRequests[player.PlayerId] = true;
         }
     }
 }
