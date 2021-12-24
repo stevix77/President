b76ab6bbf904cf43a3c39430a7c5eda15f983214
stevix77 <s.valentin77@gmail.com>
2021-12-24 01:42:39 +0100

    GameStartedShouldInitializedCards

---
+++ President.Application/Usecases/DistributeCards/DistributeCardsCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using President.Domain.Games;
+using President.Domain.Players;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace President.Application.Usecases.DistributeCards
+{
+    public class DistributeCardsCommandHandler
+    {
+        private readonly IGameRepository gameRepository;
+        private readonly IPlayerRepository playerRepository;
+
+        public DistributeCardsCommandHandler(IGameRepository gameRepository, IPlayerRepository playerRepository)
+        {
+            this.gameRepository = gameRepository;
+            this.playerRepository = playerRepository;
+        }
+
+        public async Task Handle(DistributeCardsCommand command)
+        {
+            var game = await gameRepository.GetByIdAsync(new GameId("g1"));
+            game.Distribute(Array.CreateInstance(typeof(object), 52).Cast<object>().ToArray());
+            await gameRepository.SaveAsync(game);
+        }
+    }
+}
