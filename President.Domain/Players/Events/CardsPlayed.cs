8a49a8312322c3f3cda117bf2045c3730970ead0
stevix77 <s.valentin77@gmail.com>
2021-12-27 23:35:31 +0100

    add CardsPlayedEvent

---
+++ President.Domain/Players/Events/CardsPlayed.cs
@@ -0,0 +1,16 @@
+﻿using President.Domain.Games;
+
+namespace President.Domain.Players.Events
+{
+    class CardsPlayed : IDomainEvent
+    {
+        private readonly PlayerId _playerId;
+        private readonly GameId _gameId;
+
+        public CardsPlayed(PlayerId playerId, GameId gameId)
+        {
+            _playerId = playerId;
+            _gameId = gameId;
+        }
+    }
+}
