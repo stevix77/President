8bd1cb80da6bf8b52c07c1db7fe4062f6dc81128
stevix77 <s.valentin77@gmail.com>
2021-12-28 00:13:02 +0100

    SkipTurnShouldChangePlayer

---
+++ President.Application/Usecases/Skip/SkipCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace President.Application.Usecases.Skip
+{
+    public class SkipCommand
+    {
+        public SkipCommand(string gameId, string playerId)
+        {
+            GameId = gameId;
+            PlayerId = playerId;
+        }
+
+        public string GameId { get; }
+        public string PlayerId { get; }
+    }
+}
